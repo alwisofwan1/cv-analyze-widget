50cdbabbe0b933fe11f30b3d71b1ed415efa7701
alwisofwan <alwisofwan82@gmail.com>
2025-05-25 21:33:44 +0700

    change html widget url

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/widget.tsx'),
-      name: 'MyWidget',
-      formats: ['iife'],
-      fileName: () => 'my-widget.js',
-    },
-    rollupOptions: {
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()]
 });
